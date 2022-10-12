4a7479bbb3d6130975aa4729e2c483741b4457aa
Nam <nxnamvn@gmail.com>
2022-10-12 14:42:02 +0700

    update code

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Dependencies",
-            dependencies: []),
+            dependencies: ["Utils"]),
         .testTarget(
             name: "DependenciesTests",
             dependencies: ["Dependencies"]),
--- Tests/DependenciesTests/DependenciesTests.swift
+++ Tests/DependenciesTests/DependenciesTests.swift
@@ -6,6 +6,6 @@ final class DependenciesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Dependencies().text, "Hello, World!")
+        XCTAssertNotNil(Dependencies.shared.personalModule)
     }
 }
